222435b2ac7423a2a5f716fc05b5c8ea728442c1
huangca <hch0612@gmail.com>
2020-05-19 19:36:27 -0700

    heroku#1

--- src/models/city.js
+++ src/models/city.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const REACT_APP_API_URL = "http://localhost:3001/api/v1"
+//const REACT_APP_API_URL = "http://localhost:3001/api/v1"
+const REACT_APP_API_URL = "https://sheltered-brushlands-22749.herokuapp.com/api/v1"
 
 export default class UserModel {
 	static getAllCities(){
